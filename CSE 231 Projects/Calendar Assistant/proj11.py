6e241f2931ac96e11d9b591632e1939705a84ba6
logan-the-engineer <111250525+logan-the-engineer@users.noreply.github.com>
2022-08-16 20:30:01 -0400

    Update proj11.py

--- CSE 231 Projects/Calendar Assistant/proj11.py
+++ CSE 231 Projects/Calendar Assistant/proj11.py
@@ -3,7 +3,8 @@ CSE Project #11 - Calendar Assistant
 
 Description
 
-    This calendar assistant records and updates events. It is 
+    This calendar assistant records and updates events. The
+    assistant is used within the Python Interpreter. It is 
     implemented using two classes and a driver. It uses a
     24-hour clock and mm/dd/yyyy date formatting. For a
     detailed description, read "Project11.pdf"
